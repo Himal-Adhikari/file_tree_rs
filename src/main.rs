652ee047a3131efff7c537aaa68cd40c448168b5
Himal Adhikari <himalad321@gmail.com>
2023-04-21 19:47:18 +0545

    it works

--- src/main.rs
+++ src/main.rs
@@ -61,20 +61,36 @@ fn display_tree(files: Vec<Files>, position: usize, mut are_final: &mut Vec<bool
             Files::File(file_name) => {
                 let ending_pattern = {
                     if file_position == total_files {
+                        are_final[position] = true;
                         "└──"
                     } else {
+                        are_final[position] = false;
                         "├──"
                     }
                 };
                 if position == 0 {
                     println!("{} {}", ending_pattern, file_name.into_string().unwrap());
                 } else {
+                    let mut res = String::new();
+                    for &bl in are_final.iter().take(position) {
+                        if !bl {
+                            res.push_str("│  ");
+                        } else {
+                            res.push_str("   ");
+                        }
+                    }
                     println!(
-                        "│{}{} {}",
-                        " ".repeat((position - 1) * 3 + 2),
+                        "{}{} {}",
+                        res,
                         ending_pattern,
                         file_name.into_string().unwrap()
                     );
+                    // println!(
+                    //     "│{}{} {}",
+                    //     " ".repeat((position - 1) * 3 + 2),
+                    //     ending_pattern,
+                    //     file_name.into_string().unwrap()
+                    // );
                 }
             }
             Files::Directory(dir) => {
@@ -84,33 +100,62 @@ fn display_tree(files: Vec<Files>, position: usize, mut are_final: &mut Vec<bool
                             are_final[position] = true;
                             "└──"
                         } else {
-                            "├── "
+                            are_final[position] = false;
+                            "├──"
                         }
                     };
                     println!("{} {}", ending_pattern, dir.name.into_string().unwrap());
                     match are_final.get(position + 1) {
                         Some(_) => (),
-                        None => are_final.push(false), // Default value, will be changed later
+                        None => {
+                            are_final.push(dir.files.len() > 1);
+                        } // Default value, will be changed later
                     }
                     display_tree(dir.files, position + 1, are_final);
                 } else {
                     if file_position == total_files {
                         are_final[position] = true;
+                    } else {
+                        are_final[position] = false;
                     }
                     let no_of_files = dir.files.len();
                     let ending_pattern = {
-                        if no_of_files > 0 || file_position == total_files {
+                        if (no_of_files > 0 && are_final[position]) || file_position == total_files
+                        {
                             "└──"
                         } else {
                             "├──"
                         }
                     };
+                    let mut starting_pattern = String::new();
+                    for &is_final in are_final.iter().take(position) {
+                        if !is_final {
+                            starting_pattern.push_str("│  ");
+                        } else {
+                            starting_pattern.push_str("   ");
+                        }
+                    }
+
                     println!(
-                        "│{}{} {}",
-                        " ".repeat((position - 1) * 3 + 2),
+                        "{}{} {}",
+                        starting_pattern,
                         ending_pattern,
                         dir.name.into_string().unwrap()
                     );
+                    // let starting_char = {
+                    //     if are_final[0] {
+                    //         " "
+                    //     } else {
+                    //         "│"
+                    //     }
+                    // };
+                    // println!(
+                    //     "{}{}{} {}",
+                    //     starting_char,
+                    //     " ".repeat((position - 1) * 3 + 2),
+                    //     ending_pattern,
+                    //     dir.name.into_string().unwrap()
+                    // );
                     match are_final.get(position + 1) {
                         Some(_) => (),
                         None => are_final.push(false), // Default value, will be changed later
